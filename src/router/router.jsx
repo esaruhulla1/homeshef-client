4d35872411ddb0123552a859df7be3a5d6205b24
Esa <esaruhulla01@gmail.com>
2025-12-06 17:51:52 +0600

    add initial config

---
+++ src/router/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        element: <p>main</p>
+      },
+    ]
+  },
+
+]);
